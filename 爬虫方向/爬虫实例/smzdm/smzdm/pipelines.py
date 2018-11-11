dfec038ee13ea2876084816cb99cf68cf78c2a3b
liurongsheng <308837162@qq.com>
2018-11-11 15:18:28 +0800

    :memo: 优化双十一爬虫代码

--- 爬虫方向/爬虫实例/smzdm/smzdm/pipelines.py
+++ 爬虫方向/爬虫实例/smzdm/smzdm/pipelines.py
@@ -1,37 +1,46 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
 from smzdm.items import SmzdmItem
 import pymongo
 import pymysql
 
 class SmzdmMySQLPipeline(object):
+    db = pymysql.connect(
+        host="127.0.0.1",
+        user='root',
+        password='admin123',
+        database='pymysql',
+        port=3306
+    )
+
     def __init__(self):
-        pass
+        cursor = self.db.cursor()
+        cursor.execute("DROP TABLE IF EXISTS SMZDM")
+        sqlinit = """CREATE TABLE SMZDM (
+             id int(20) NOT NULL AUTO_INCREMENT,
+             title varchar(256),
+             href varchar(256),  
+             referrerName varchar(50),
+             starNum int(20), 
+             againStarNum int(20), 
+             pushTime varchar(20), 
+             fromWhere varchar(20),
+             PRIMARY KEY (`id`,`href`) USING BTREE 
+             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+         """
+        cursor.execute(sqlinit)
+        self.db.commit()
+        cursor.close()
 
     def process_item(self, item, spider):
-        db = pymysql.connect(
-            host="127.0.0.1",
-            user='root',
-            password='admin123',
-            database='pymysql',
-            port=3306
-        )
-        cursor = db.cursor()
+        cursor = self.db.cursor()
         sql = """
-            insert into smzdm(
+            insert into SMZDM(
                 id,title,href,referrerName,starNum,againStarNum,pushTime,fromWhere
               ) 
               values(null,%s,%s,%s,%s,%s,%s,%s);
         """
         cursor.execute(sql,[item['title'],item['href'],item['referrerName'],item['starNum'],item['againStarNum'],item['pushTime'],item['fromWhere']])
-        db.commit()
+        self.db.commit()
         cursor.close()
-
 
 class SmzdmMongoDBPipeline(object):
     def __init__(self):
--- 爬虫方向/爬虫实例/smzdm/smzdm/spiders/zdm.py
+++ 爬虫方向/爬虫实例/smzdm/smzdm/spiders/zdm.py
@@ -5,13 +5,15 @@ from smzdm.items import SmzdmItem
 class SmzdmSpider(scrapy.Spider):
     name = 'smzdm'
     allowed_domains = ['www.smzdm.com']
-    # base_url = 'http://www.smzdm.com'
     start_urls = ['https://www.smzdm.com/p1/']
-    searchName = '当当优品'
+    # base_url = 'http://www.smzdm.com'
+    searchName = "绝对值"
+
     def parse(self, response):
         mainLlist = response.xpath("//ul[@class='feed-list-hits']//li")
         for list in mainLlist:
             title = list.xpath(".//h5/a/text()").get()
+
             href = list.xpath(".//h5/a/@href").get()
             referrerName = list.xpath(".//div[@class='feed-block-info']//span/text()").get()
             starNum = list.xpath(".//div[@class='z-feed-foot-l']//a[1]//span[@class='unvoted-wrap']/span/text()").get()
@@ -19,18 +21,29 @@ class SmzdmSpider(scrapy.Spider):
             pushTime = list.xpath(".//div[@class='z-feed-foot-r']//span/text()").getall()
             pushTime = "".join(pushTime).strip()
             fromWhere = list.xpath(".//div[@class='z-feed-foot-r']//span/a/text()").get()
-            res = re.search(self.searchName,title)
-            if res == None:
-                print(title)
-            else:
+
+            # 正则匹配抓取模式
+            # if title != None:
+            #     res = re.search(self.searchName, title) # 正则匹配到关键字才抓取的方法
+            # if res != None:
+            #     print('获取到一条数据：' + title)
+            #     item = SmzdmItem(title=title, href=href, referrerName=referrerName, starNum=starNum,
+            #                      againStarNum=againStarNum, pushTime=pushTime, fromWhere=fromWhere, )
+            #     yield item
+            # else:
+            #     print(title)
+
+            # 抓取全部数据
+            if title != None:
                 print('获取到一条数据：'+title)
                 item = SmzdmItem(title=title,href=href,referrerName=referrerName,starNum=starNum,
                                  againStarNum=againStarNum,pushTime=pushTime,fromWhere=fromWhere,)
                 yield item
 
-        next_url = response.xpath("//div[@class='feed-pagenation']//ul//li[last()]//a/@href").get()
+        next_url = response.xpath("//a[text()='下一页']/@href").get()
         print('*'*100)
         if not next_url:
             yield scrapy.Request('https://www.smzdm.com/p2/', callback=self.parse)
+            # 第一页开始会找到第二页，从第二页开始没有问题
         else:
             yield scrapy.Request(next_url,callback=self.parse)
