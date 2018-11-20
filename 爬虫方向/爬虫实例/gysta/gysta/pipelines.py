1ca06d98848b61cd8c3eebe1e0543b9fa3623e56
liurongsheng <308837162@qq.com>
2018-11-20 16:27:51 +0800

    :memo: 整合SQL到pipeline

--- 爬虫方向/爬虫实例/gysta/gysta/pipelines.py
+++ 爬虫方向/爬虫实例/gysta/gysta/pipelines.py
@@ -3,18 +3,46 @@ import pymongo
 from gysta.items import GystaItem
 
 class GystaPipeline(object):
+    db = pymysql.connect(
+        host="127.0.0.1",
+        user='root',
+        password='admin123',
+        database='pymysql',
+        port=3306
+    )
+
     def __init__(self):
-        pass
+        cursor = self.db.cursor()
+        cursor.execute("DROP TABLE IF EXISTS em_tours")
+        sqlinit = """CREATE TABLE `em_tours` (
+                      `ID` int(10) NOT NULL AUTO_INCREMENT,
+                      `AMOUNT` varchar(255) DEFAULT NULL,
+                      `TOURNAME` varchar(255) DEFAULT NULL,
+                      `BUSNUM` varchar(255) DEFAULT NULL,
+                      `GUIDECARDNUM` varchar(255) DEFAULT NULL,
+                      `TRAVELAGENCYCODE` varchar(255) DEFAULT NULL,
+                      `STARTDATE` varchar(255) DEFAULT NULL,
+                      `ENDDATE` varchar(255) DEFAULT NULL,
+                      `DESTINATION` varchar(255) DEFAULT NULL,
+                      `TOURNUM` varchar(255) DEFAULT NULL,
+                      `TOURTYPE` varchar(255) DEFAULT NULL,
+                      `SOURCE` varchar(255) DEFAULT NULL,
+                      `STATUS` varchar(255) DEFAULT NULL,
+                      `CSTATUS` varchar(255) DEFAULT NULL,
+                      `REGION` varchar(255) DEFAULT NULL,
+                      `TOURTYPE1` varchar(255) DEFAULT NULL,
+                      `ORIGIN` varchar(255) DEFAULT NULL,
+                      `TOURLINE` varchar(255) DEFAULT NULL,
+                      `HOTEL` varchar(255) DEFAULT NULL,
+                      PRIMARY KEY (`ID`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8MB4;
+                 """
+        cursor.execute(sqlinit)
+        self.db.commit()
+        cursor.close()
 
     def process_item(self, item, spider):
-        db = pymysql.connect(
-            host="127.0.0.1",
-            user='root',
-            password='admin123',
-            database='pymysql',
-            port=3306
-        )
-        cursor = db.cursor()
+        cursor = self.db.cursor()
         print(item['TOURNAME'])
         sql = """
             insert into em_tours(
@@ -26,27 +54,8 @@ class GystaPipeline(object):
         print('++++++++++++++++++++++++++++++++++')
         print(data)
         cursor.execute(sql,data)
-        db.commit()
+        self.db.commit()
         cursor.close()
-
-    # def process_item(self, item, spider):
-    #         db = pymysql.connect(
-    #             host="127.0.0.1",
-    #             user='root',
-    #             password='admin123',
-    #             database='pymysql',
-    #             port=3306
-    #         )
-    #         cursor = db.cursor()
-    #         sql = """
-    #             insert into test(
-    #                 ID,title
-    #               )
-    #               values(null,%s);
-    #         """
-    #         cursor.execute(sql,['liu'])
-    #         db.commit()
-    #         cursor.close()
 
 class GystaMongoDBPipeline(object):
     def __init__(self):
@@ -60,28 +69,3 @@ class GystaMongoDBPipeline(object):
             except Exception:
                 print("出错了++++++++++++++++++++++++++++++++++++++++++++++")
         return item
-
-
-# mySQL 脚本
-# CREATE TABLE `em_tours` (
-#   `ID` int(10) NOT NULL AUTO_INCREMENT,
-#   `AMOUNT` varchar(255) DEFAULT NULL,
-#   `TOURNAME` varchar(255) DEFAULT NULL,
-#   `BUSNUM` varchar(255) DEFAULT NULL,
-#   `GUIDECARDNUM` varchar(255) DEFAULT NULL,
-#   `TRAVELAGENCYCODE` varchar(255) DEFAULT NULL,
-#   `STARTDATE` varchar(255) DEFAULT NULL,
-#   `ENDDATE` varchar(255) DEFAULT NULL,
-#   `DESTINATION` varchar(255) DEFAULT NULL,
-#   `TOURNUM` varchar(255) DEFAULT NULL,
-#   `TOURTYPE` varchar(255) DEFAULT NULL,
-#   `SOURCE` varchar(255) DEFAULT NULL,
-#   `STATUS` varchar(255) DEFAULT NULL,
-#   `CSTATUS` varchar(255) DEFAULT NULL,
-#   `REGION` varchar(255) DEFAULT NULL,
-#   `TOURTYPE1` varchar(255) DEFAULT NULL,
-#   `ORIGIN` varchar(255) DEFAULT NULL,
-#   `TOURLINE` varchar(255) DEFAULT NULL,
-#   `HOTEL` varchar(255) DEFAULT NULL,
-#   PRIMARY KEY (`ID`)
-# ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
